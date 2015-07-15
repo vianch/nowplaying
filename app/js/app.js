1f1df47ce8a7a141730ce71b86afcb567ddaa0df
Victor Chavarro <info@vianch.com>
2015-07-14 23:53:19 -0500

    angular route
    now the index load home.html with ngRoute

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,11 @@
 import angular from 'angular';
+import 'angular-route';
 
-angular.module('NowPlayingApp', [
-
-]);
+angular.module('NowPlayingApp', ['ngRoute'])
+    .config(['$routeProvider',function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'templates/pages/home.html'
+            })
+            .otherwise({ redirectTo: '/' });
+}]);
